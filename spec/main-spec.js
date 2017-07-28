35bdfd71a29c49227c34333af07e878a113c559f
Alexandre Voiney <pro@alexandre.voiney.fr>
2017-07-28 13:15:47 +0200

    Fix activationHooks issue

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -5,6 +5,7 @@ describe('The linter-spell-rst provider for Atom Linter', () => {
   const grammar = require('../lib/main').provideGrammar()[0]
 
   beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace)
     waitsForPromise(() => {
       return atom.packages.activatePackage('linter-spell-rst')
     })
